87a7de75aa5e174fab4bebd3246ac412b949e153
THRILLHO <K8297@student.jamk.fi>
2017-01-29 15:18:42 +0200

    ehostettu teht 6

--- HockeyApplication/Hockey.cs
+++ HockeyApplication/Hockey.cs
@@ -13,16 +13,23 @@ namespace HockeyApplication
         public string TeamName { get; set; }
         public int Wins { get; set; }
         public int Losses { get; set; }
+        public int Ties { get; set; }
         public void WinGame()
         {
-            Console.WriteLine("You win a game! Well done.");
+            Console.WriteLine("You win the game! Well done.");
             Wins += 1;
             Points += 2;
         }
         public void LoseGame()
         {
-            Console.WriteLine("You lose a game...better luck next time, I guess.");
+            Console.WriteLine("You lose the game...better luck next time.");
             Losses += 1;
+        }
+        public void TieGame()
+        {
+            Console.WriteLine("The game ended in a tie.");
+            Ties += 1;
+            Points += 1;
         }
         public void CheckStat()
         {
@@ -34,9 +41,28 @@ namespace HockeyApplication
             {
                 Console.WriteLine((Wins + Losses) + " games played.");
             }
-            Console.WriteLine(TeamName + "'s stats: " + Wins + "-" + Losses);
+            Console.WriteLine(TeamName + "'s stats: " + Wins + "-" + Losses + "-" + Ties);
             Console.WriteLine(TeamName + " has " + Points + " points.");
             
+        }
+        public void GMChat()
+        {
+            if ((Wins + Losses) == 0)
+            {
+                Console.WriteLine("We haven't played a single game yet. My hopes are high for this season.");
+            }
+            else if (Losses > Wins)
+            {
+                Console.WriteLine("We are underperforming. Maybe we'll have to make some moves...");
+            }
+            else if (Wins > Losses)
+            {
+                Console.WriteLine("Seems like we're doing decently so far. Hopefully the guys can keep this up.");
+            }
+            else if (Wins == Losses)
+            {
+                Console.WriteLine("We are 50%. This is not enough to make the playoffs, so we'll need to step it up.");
+            }
         }
     }
       
--- HockeyApplication/Program.cs
+++ HockeyApplication/Program.cs
@@ -12,31 +12,63 @@ namespace HockeyApplication
         {
             int choice = 0;
             Hockey hockey = new Hockey();
+            Random random = new Random();
             hockey.Points = 0;
             hockey.Wins = 0;
             hockey.Losses = 0;
+            hockey.Ties = 0;
             Console.WriteLine("Welcome to Late's hockey simulator. Pick a name for your team!");
             string line = Console.ReadLine();
             hockey.TeamName = line;
             do
             {
-                Console.WriteLine(hockey.TeamName + " 's regular season is underway. What would you like to do?");
-                Console.WriteLine("1. Win a game!");
-                Console.WriteLine("2. Lose a game...");
-                Console.WriteLine("3. Check your stats");
+                int enscore = random.Next(9);
+                int youscore = random.Next(9);
+                Console.WriteLine(hockey.TeamName + "'s regular season is underway. What would you like to do?");
+                //Console.WriteLine("1. Win a game!");
+                //Console.WriteLine("2. Lose a game...");
+                Console.WriteLine("1. Check your stats");
+                Console.WriteLine("2. Check with your GM");
+                Console.WriteLine("3. Play next game!");
                 string line2 = Console.ReadLine();
                 choice = int.Parse(line2);
-                if (choice == 1)
+               /* if (choice == 1)
                 {
                     hockey.WinGame();
                 }
                 else if (choice == 2)
                 {
                     hockey.LoseGame();
+                }*/
+                if (choice == 1)
+                {
+                    hockey.CheckStat();
+                }
+                else if (choice == 2) 
+                {
+                    hockey.GMChat();
                 }
                 else if (choice == 3)
                 {
-                    hockey.CheckStat();
+                    Console.WriteLine("You play in a game. Good luck!");
+                    Console.WriteLine("The game has ended.");
+                    Console.WriteLine(hockey.TeamName + ": " + youscore + ", Opponent: " + enscore);
+                    if (youscore > enscore)
+                    {
+                        hockey.WinGame();
+                    }
+                    else if (enscore > youscore)
+                    {
+                        hockey.LoseGame();
+                    }
+                    else if (enscore == youscore)
+                    {
+                        hockey.TieGame();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid option.");
                 }
 
             } while ((hockey.Wins + hockey.Losses) < 82);
